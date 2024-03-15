b19fa222f2a6ad8ea1c89e14c16ea1bb9a02b7e6
Lucas <lucasfbustamante@gmail.com>
2024-03-15 10:40:29 -0300

    Create example-public-plugin.php

---
+++ example-public-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Example Public WordPress Plugin
+ * Plugin URI: https://example.com/plugin
+ * Description: This is a simple example of a WordPress plugin. Used for testing QIT Environment Custom Handlers.
+ * Version: 1.0
+ * Author: Your Name
+ * Author URI: https://example.com
+ * License: GPLv2 or later
+ * Text Domain: example-plugin
+ */
